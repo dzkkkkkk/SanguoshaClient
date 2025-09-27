acb314d51d4c5b65dcc108984237a708a83e660c
dzkkkkkk <1905682870@qq.com>
2025-09-27 16:11:45 +0800

    成功进入大厅且字体无乱码

--- main.cpp
+++ main.cpp
@@ -1,26 +1,38 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QTextCodec>
 #include <QFont>
 #include <QFontDatabase>
+#include "mainwindow.h"  // 确保包含 MainWindow 的头文件
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    
-    // 设置应用程序字体，优先使用微软雅黑，其次是文泉驿微米黑
+
+    // 设置编码为UTF-8
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+#else
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+#endif
+
+    // 创建 QFontDatabase 对象
+    QFontDatabase fontDatabase;
+
+    // 设置应用程序字体
     QFont font;
-    if (QFontDatabase::families().contains("Microsoft YaHei")) {
+    if (fontDatabase.families().contains("Microsoft YaHei")) {
         font.setFamily("Microsoft YaHei");
-    } else if (QFontDatabase::families().contains("WenQuanYi Micro Hei")) {
+    } else if (fontDatabase.families().contains("WenQuanYi Micro Hei")) {
         font.setFamily("WenQuanYi Micro Hei");
     } else {
-        // 使用系统默认字体，但设置合适的字号
         font = QApplication::font();
     }
     font.setPointSize(9);
     QApplication::setFont(font);
-    
-    MainWindow w;
+
+    MainWindow w;  // 确保 MainWindow 类已正确声明
     w.show();
     return a.exec();
 }
